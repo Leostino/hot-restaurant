a3f7951124c7b550c6232f37f1f2c89bf4e976e9
Lav Patel <lavpatel9507@gmail.com>
2019-09-07 17:07:38 -0400

    edit to the js file

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-var bodyParser = require('body-parser')
 var express = require('express')
-var path = require('path');
+
 
 const app = express();
 var PORT = process.env.PORT || 3000;
- 
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// application/json
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-require('./applications/routes/api-route.js')(app);
-require('./applications/routes/html-route.js')(app);
+require('./routes/api-route')(app);
+require('./routes/html-route')(app);
 
 
 
