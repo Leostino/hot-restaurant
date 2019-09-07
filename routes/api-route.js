984ae0f6310ff8c3140bad91b1e8a78d1ae77d91
Lav Patel <lavpatel9507@gmail.com>
2019-09-07 17:38:28 -0400

    change file format

---
+++ routes/api-route.js
@@ -0,0 +1,25 @@
+var tableData = require('../tableinfo/table-data.js');
+var waitListData = require('../tableinfo/waitlist-data.js');
+module.exports = function (app) {
+    app.get('/api/table', function(req, res) {
+        res.json(tableData);
+    });
+    app.get('/api/waitlist', function(req, res) {
+        res.json(waitListData);
+    });
+    app.post('/api/tables', function(req, res)   {
+        if(tableData.length < 5)    {
+            tableData.push(req.body);
+            res.json(true);
+        } else  {
+            waitListData.push(req.body);
+            res.json(false);
+        }
+    });
+    app.post('/api/clear', function()   {
+        tableData = [];
+        waitListData = [];
+        console.log(tableData);
+        console.log(waitListData);
+    })
+}
